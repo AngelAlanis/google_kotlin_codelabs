abbf0f2861bde1803f132d479f4b58b6270d7ed3
ahangel256@gmail.com <ahangel256@gmail.com>
2023-04-07 17:39:42 -0600

    Example of a lambda function

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,30 +1,12 @@
 fun main() {
-    val peopleAges = mutableMapOf<String, Int>(
-        "Fred" to 30,
-        "Ann" to 23
-    )
-    println(peopleAges)
+    val triple: (Int) -> Int = { it * 3 }
+    println(triple(5))
 
-    // Two ways of adding values to the map
-    peopleAges.put("Barbara", 42)
-    peopleAges["Joe"] = 51
+    val peopleNames = listOf("Fred", "Ann", "Barbara", "Joe")
+    println(peopleNames.sorted())
 
-    // Replacing the value of the key 'Fred'
-    peopleAges["Fred"] = 31
-
-    println(peopleAges)
-
-    // Make a forEach operation
-    peopleAges.forEach { print("${it.key} is ${it.value}, ") }
-
-    println()
-
-    // Apply a map transformation for each item and convert it to a string
-    println(peopleAges.map { "${it.key} is ${it.value}" }.joinToString(", "))
-
-    // Filter operation
-    // Filter keys (names) to show only those with less than 4 characters
-    val filteredNames = peopleAges.filter { it.key.length < 4 }
-    print(filteredNames)
-
+    // Sort names by length
+    println(peopleNames.sortedWith { str1: String, str2: String ->
+        str1.length - str2.length
+    })
 }
